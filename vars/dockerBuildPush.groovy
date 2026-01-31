2224c0e8536c87f17de90713c8274d3288e89564
kshitijx07 <166832880+kshitijx07@users.noreply.github.com>
2026-01-31 23:39:44 +0530

    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -3,13 +3,12 @@ def call(Map config) {
     def imageTag = "${config.user}/${config.image}:${config.version}"
     def latest   = "${config.user}/${config.image}:latest"
 
-    def buildDir = config.dir?.trim()
-    if (!buildDir) {
-        buildDir = '.'
-    }
+    def buildDir = config.dir?.trim() ?: '.'
 
-    sh "docker build --network=host -t ${imageTag} ${buildDir}"
-    sh "docker tag ${imageTag} ${latest}"
-    sh "docker push ${imageTag}"
-    sh "docker push ${latest}"
+    sh """
+      docker build --network=host -t ${imageTag} ${buildDir}
+      docker tag ${imageTag} ${latest}
+      docker push ${imageTag}
+      docker push ${latest}
+    """
 }
