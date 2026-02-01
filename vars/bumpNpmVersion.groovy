695ec1d8550a389243336be0f781b64b2075e0f0
kshitijx07 <166832880+kshitijx07@users.noreply.github.com>
2026-02-01 09:51:35 +0530

    Require 'dir' parameter in bumpNpmVersion function
    Ensure 'dir' is provided in the config map.

--- vars/bumpNpmVersion.groovy
+++ vars/bumpNpmVersion.groovy
@@ -1,11 +1,11 @@
 def call(Map config) {
-
     if (!config.dir) {
         error "bumpNpmVersion: dir is REQUIRED"
     }
 
     return sh(
         script: """
+            set -e
             cd ${config.dir}
             npm version patch --no-git-tag-version
             node -p "require('./package.json').version"
